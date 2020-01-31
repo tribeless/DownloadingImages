39314bc494c9429f390118b2773f5bc18b21f6bb
Brian Kyole <tribeless53@gmail.com>
2020-01-31 12:37:56 +0300

    first commit

---
+++ app/src/main/java/com/example/downloadingimages/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.downloadingimages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+
+
+/*
+********************************************************
+********************************************************
+************** CODE TO DOWNLOAD AN**********************
+***************IMAGE FROM THE INTERNET******************
+********************************************************
+********************************************************
+********************************************************
+***/
+public class MainActivity extends AppCompatActivity {
+
+    Button clickedButton;
+    ImageView myImage;
+
+    //function to download image
+    public void downloadImage(View view) {
+
+
+        //object creation for class ImageDownloader
+       ImageDownloader imageDownloader = new ImageDownloader();
+       Bitmap picture;
+        try {
+            picture = imageDownloader.execute("https://d3hne3c382ip58.cloudfront.net/files/uploads/bookmundi/resized/cmsfeatured/places-to-travel-in-2018-1522385995-390X219.jpg").get();
+            myImage.setImageBitmap(picture);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        clickedButton = findViewById(R.id.button);
+        myImage = findViewById(R.id.imageView);
+
+
+    }
+
+
+    //class extending AsyncTask to run on a background thread
+
+       public static class ImageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+           protected Bitmap doInBackground(String...urls){
+
+            URL url;
+            try {
+                url = new URL(urls[0]);
+
+                //opening the connection to the browser
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                //downloading the image directly and then converting it into a bitmap
+                InputStream inputStream = urlConnection.getInputStream();
+                return BitmapFactory.decodeStream(inputStream);
+            }
+
+            catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+
+       }
+
+}
